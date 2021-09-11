2ea8bb2fb850cae05598dbe22abaedf0cbb52d27
Ivan Longarai <ivanslongarai@gmail.com>
2021-09-11 10:14:33 -0300

    First Event Controller

---
+++ Back/src/ProEventos.API/Controllers/EventController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventController : ControllerBase
+    {
+
+        public EventController()
+        {
+            
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Value";
+        }
+    }
+}
